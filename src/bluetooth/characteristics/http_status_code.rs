ee1b8e1fc44d7ee42fd41faf6ac19ea320065cad
mune <munerato.ale@gmail.com>
2024-08-27 15:17:10 +0200

    fix: Event listener creation now behaves correctly on a new subscription

--- src/bluetooth/characteristics/http_status_code.rs
+++ src/bluetooth/characteristics/http_status_code.rs
@@ -7,6 +7,7 @@ use tracing::{debug, warn};
 
 pub fn create_characteristic(state: &Arc<AppState>) -> Characteristic {
     let state_r = state.clone();
+    let event_listeners_queue = Arc::new(Mutex::new(Vec::<String>::new()));
     Characteristic {
         uuid: *HTTP_STATUS_CODE_UUID,
         read: Some(CharacteristicRead {
@@ -26,26 +27,33 @@ pub fn create_characteristic(state: &Arc<AppState>) -> Characteristic {
             notify: true,
             method: CharacteristicNotifyMethod::Fun(Box::new(move |notifier| {
                 let notifier = Arc::new(Mutex::new(notifier));
+                let listeners = event_listeners_queue.clone();
                 async move {
-                   let listeners = EVENT_EMITTER.lock().await.listeners.len();
-                   debug!("Listeners for event {}: {}", HTTP_STATUS_CODE_UPDATED_EVENT, listeners);
-                   if listeners == 0 {
-                        debug!("Initializing event {}", HTTP_STATUS_CODE_UPDATED_EVENT);
-                        EVENT_EMITTER.lock().await.on(HTTP_STATUS_CODE_UPDATED_EVENT, move |value: Vec<u8>| {
-                            debug!("Event {} triggered", HTTP_STATUS_CODE_UPDATED_EVENT);
-                            let notifier = notifier.clone();
-                            debug!("Notifying with value {:x?}", &*value);
-                            futures::executor::block_on(async move {
-                                let mut notifier = notifier.lock().await;
-                                if let Err(err) = notifier.notify(value).await {
-                                    warn!("Notification error: {}", &err);
-                                    return;
-                                }
-                                debug!("Notification sent")
-                            });
+                    let mut listeners = listeners.lock().await;
+                    if !listeners.is_empty() {
+                        for listener in listeners.iter() {
+                            debug!("Removing listener {}", listener);
+                            EVENT_EMITTER.lock().await.remove_listener(listener);
+                            debug!("Listener {} removed successfully", listener);
+                        }
+                    }
+                    let notifier = notifier.clone();
+                    debug!("Initializing event {}", HTTP_STATUS_CODE_UPDATED_EVENT);
+                    let event = EVENT_EMITTER.lock().await.on(HTTP_STATUS_CODE_UPDATED_EVENT, move |value: Vec<u8>| {
+                        debug!("Event {} triggered", HTTP_STATUS_CODE_UPDATED_EVENT);
+                        let notifier = notifier.clone();
+                        debug!("Notifying with value {:x?}", &*value);
+                        futures::executor::block_on(async move {
+                            let mut notifier = notifier.lock().await;
+                            if let Err(err) = notifier.notify(value).await {
+                                warn!("Notification error: {}", &err);
+                                return;
+                            }
+                            debug!("Notification sent");
                         });
-                        debug!("Event {} intialized successfully", HTTP_STATUS_CODE_UPDATED_EVENT);       
-                   }
+                    });
+                    *listeners = vec![event.clone()];
+                    debug!("Event {} with id {} intialized successfully", HTTP_STATUS_CODE_UPDATED_EVENT, event);       
                 }
                 .boxed()
             })),
